2d0a978be45968e20b0b7505547c52e8bf101f8a
Thiago Barcellos Mattos <th.mattos04@gmail.com>
2018-05-06 11:30:15 -0300

    Initial commit

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour {
+
+	public GameObject menu;
+	static bool isPaused = false;
+
+	// Use this for initialization
+	void Start () {
+		menu.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (isPaused == false) {
+				Time.timeScale = 0;
+				menu.SetActive (true);
+				isPaused = true;
+			} else {
+				Time.timeScale = 1;
+				menu.SetActive (false);
+				isPaused = false;
+			}
+
+		}
+	}
+}
