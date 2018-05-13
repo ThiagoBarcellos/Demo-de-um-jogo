ff3782e6dde556740681b7b24f215a8e66e00497
Ana Júlia Borges <aninhadas@gmail.com>
2018-05-13 20:02:05 -0300

    Demo de um jogo
    Scripts e sons recebidos adicionados

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour {
+
+	public GameObject menu;
+	static bool isPaused = false;
+
+	public GameObject panelCP, panelSC, panelCont, panelEmail;
+	public Button contact, email, exit1, exit2, play, menuB, Pplay;
+	bool isActiveCP = false;
+	bool isActiveSC = false;
+
+	void Start () {
+
+		menu.SetActive (false);
+		panelCP.SetActive (false);
+		panelSC.SetActive (false);
+		panelCont.SetActive (false);
+		panelEmail.SetActive (false);
+
+		contact = contact.GetComponent<Button> ();
+		email = email.GetComponent<Button> ();
+		exit1 = exit1.GetComponent<Button> ();
+		exit2 = exit2.GetComponent<Button> ();
+		play = play.GetComponent<Button> ();
+		menuB = menuB.GetComponent<Button> ();
+		Pplay = Pplay.GetComponent<Button> ();
+
+		contact.onClick.AddListener (onContactsClick);
+		email.onClick.AddListener (onEmailClick);
+		exit1.onClick.AddListener (onExitCPClick);
+		exit2.onClick.AddListener (onExitCPClick);
+		play.onClick.AddListener (onPlayClick);
+		menuB.onClick.AddListener (onMenuClick);
+		Pplay.onClick.AddListener (onPrincipalPlayClick);
+
+	}
+
+	void Update ()
+	{
+		CPSC ();
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (isPaused == false) {
+				Time.timeScale = 0;
+				menu.SetActive (true);
+				isPaused = true;
+			} else {
+				Time.timeScale = 1;
+				menu.SetActive (false);
+				isPaused = false;
+			}
+
+		}
+	}
+
+	void CPSC () {
+		//turn on&off the cellphone screen
+		if (Input.GetKeyDown (KeyCode.E)) {
+			if (isActiveCP == false) {
+				panelCP.SetActive (true);
+				isActiveCP = true;
+			} else {
+				panelCP.SetActive (false);
+				isActiveCP = false;
+			}
+		}
+		//turn on&off the suitcase screen
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			
+			if (isActiveSC == false) {
+				panelSC.SetActive (true);
+				isActiveSC = true;
+
+			} else { 
+				panelSC.SetActive (false);
+				isActiveSC = false;
+
+			}
+		}
+	}
+
+	public void onPlayClick () {
+		//works just as fine as clicking Esc
+		menu.SetActive (false);
+	}
+
+	public void onMenuClick (){
+		//go back to menu. does not save your progress (yet)
+		Application.LoadLevel(0);
+	}
+
+	public void onContactsClick () {
+		//this is inside "Cellphone". Go to Contacts screen.
+		panelCont.SetActive (true);
+	}
+
+	public void onEmailClick () {
+		//this is inside "Cellphone". Go to E-mail screen.
+		panelEmail.SetActive (true);
+	}
+
+	public void onExitCPClick () {
+		//this is inside "Cellphone". Exit Contacts&Email screens.
+		panelCont.SetActive (false);
+		panelEmail.SetActive (false);
+	}
+
+	public void onPrincipalPlayClick() {
+		//play button inside Menu screen. it is in another scene.
+		Application.LoadLevel(1);
+	}
+}
--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -26,8 +26,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private float m_StepInterval;
         [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
         [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
-        [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
-
+        [SerializeField] private AudioClip m_LandSound;
+        [SerializeField] private AudioClip m_BgSound;
         private Camera m_Camera;
         private bool m_Jump;
         private float m_YRotation;
@@ -45,6 +45,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         // Use this for initialization
         private void Start()
         {
+            
             m_CharacterController = GetComponent<CharacterController>();
             m_Camera = Camera.main;
             m_OriginalCameraPosition = m_Camera.transform.localPosition;
