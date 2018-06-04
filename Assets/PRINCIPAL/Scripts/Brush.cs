1dd309fd05ccd5ef0c6cd95df470974aa5cf6854
Evelyn Correa <evelynnave2015@gmail.com>
2018-06-03 23:57:01 -0300

    03/06/2018
    - scenery on
    - fade in/end of the demo
    - clean bug solved

---
+++ Assets/PRINCIPAL/Scripts/Brush.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CircleCollider2D))]
+
+public class Brush : MonoBehaviour {
+
+	private Vector2 offset, pos;
+	public Canvas myCanvas;
+	public GameObject sujeira, fade;
+
+	void OnMouseDown() {
+
+		offset = gameObject.transform.position;
+	}
+
+	void OnMouseDrag()
+	{
+		RectTransformUtility.ScreenPointToLocalPointInRectangle (myCanvas.transform as 
+			RectTransform, Input.mousePosition, myCanvas.worldCamera, out pos);
+		
+		transform.position = myCanvas.transform.TransformPoint (pos);
+	}
+
+	void OnTriggerStay2D(Collider2D coll){
+		
+		if (coll.gameObject.CompareTag ("dirt")) {
+			StartCoroutine (limparSujeira (5f));
+		}
+	}
+
+	private IEnumerator limparSujeira(float determinadoTempo){
+
+		if (sujeira.GetComponent<CanvasGroup> ().alpha > 0f) {
+			sujeira.GetComponent<CanvasGroup> ().alpha -= 0.005f;
+			yield return new WaitForSecondsRealtime (determinadoTempo);
+		} 
+		else if (sujeira.GetComponent<CanvasGroup> ().alpha == 0) {
+			yield return new WaitForSecondsRealtime (5f);
+			fade.GetComponent<CanvasGroup> ().alpha += 0.05f;
+		}
+	}
+}
